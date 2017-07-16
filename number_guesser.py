214d361f58a0a48c1cc384d1bca8f8406b74df31
Spencer <SpencerMAQ@users.noreply.github.com>
2017-07-16 15:41:02 +0800

    Better version to catch lying humans

--- number_guesser.py
+++ number_guesser.py
@@ -4,22 +4,30 @@ Guesses what number (between 1-100) you are thinking of
 
 import random as r
 
-correct = False
-guess = r.randint(0, 100)
-guessLowerLimit = 0
-guessUpperLimit = 100
+lower_limit = 0
+upper_limit = 100
+guess = r.randint(lower_limit, upper_limit)
 
 while True:
-    print(guess)
-    inp = input("lower or higher or correct? ")
-    if inp == 'lower':
-        guessUpperLimit = guess - 1
-        guess = r.randint(guessLowerLimit, guessUpperLimit)
+    inp = input(
+        'Is the number {}?\n'
+        'higher = h, lower = l, yes = y\n'.format(guess)
+    )
 
-    elif inp == 'higher':
-        guessLowerLimit = guess + 1
-        guess = r.randint(guessLowerLimit, guessUpperLimit)
+    if inp == 'h':
+        lower_limit = guess + 1
 
-    elif inp == 'correct':
-        print("Ha, my guess was correct")
+    elif inp == 'l':
+        upper_limit = guess - 1
+
+    elif inp == 'y':
+        print('Ha! clever I am')
         break
+
+    try:
+        guess = r.randint(lower_limit, upper_limit)
+    
+    # if upper-lower = 0, it means the person has been lying somewhere
+    # The correct number was already outputed by the computer
+    except:
+        print('Human is liar, I already guessed the correct number')
